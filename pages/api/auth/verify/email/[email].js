3e98aa0f5d2d1bf5b5618c618d79aa52ff15788b
Lloyd Montero <catchphilippines@gmail.com>
2023-01-14 13:21:59 +0800

    added sendgrid on access code update

--- pages/api/auth/verify/email/[email].js
+++ pages/api/auth/verify/email/[email].js
@@ -1,3 +1,4 @@
+import sendgrid from "@sendgrid/mail";
 import connect from '../../../../../utils/connect';
 import Code from '../../../../../models/code';
 
@@ -25,6 +26,32 @@ export default async (req, res) => {
 
                 if (!findCode) return res.status(404).json({ message: "No Data Found" });
                 const updatedCode = await Code.updateOne({ _id: id }, { $set: req.body });
+
+                // send the email
+                sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+                const msg = {
+                    to: req.body.email,
+                    from: process.env.NEXT_PUBLIC_DOMAIN_FROM,
+                    subject: 'Your Verification Code - Reset Password',
+                    text: `
+                        Good day,
+                        Thank you for signing up to SynTechNX, here is you access code ${req.body.code} to the portal.
+                        If you have any problems accessing the course, please contact admin (admin@syntechnx.com)
+                        Kind Regards,
+                        Admin
+                    `,
+                    html: `
+                        <p>Good day,</p>
+                        <p>Thank you for signing up to SynTechNX, here is you access code ${req.body.code} to the portal.</p>
+                        <p>If you have any problems accessing the course, please contact admin (admin@syntechnx.com)</p><br />
+                        <p>Kind Regards,</p>
+                        <p>Admin</p>
+                    `
+                }
+
+                // Disable muna
+                await sendgrid.send(msg).catch(error => console.log('Something went wrong'));
+                
                 res.status(201).json({ success: true, data: updatedCode })
             } catch (error) {
                 res.status(400).json({ success: false });
